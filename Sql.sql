6a1f237db02a75ca665ad831e296c6c14b86f154
mayurkotkarGit <mayur,kotkar@d2kindia.com>
2025-04-28 15:25:04 +0530

    commit1

--- Sql.sql
+++ Sql.sql
@@ -2,6 +2,7 @@ use[bob_legal_plus_test]
 select
     EntityKey, UserLoginID, UserName, LoginPassword, PasswordChanged, PasswordChangeDate, ChangePwdCnt
 from DimUserInfo
+where EffectiveToTimeKey = 49999
 
 
 
