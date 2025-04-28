d80e1fa8f7140cd2540c3e2f0c78b4fb43082a6a
mayurkotkarGit <mayur,kotkar@d2kindia.com>
2025-04-28 15:11:12 +0530

    commit

--- Sql.sql
+++ Sql.sql
@@ -1,6 +1,7 @@
 use[bob_legal_plus_test]
-select 
- EntityKey,UserLoginID,UserName,LoginPassword,PasswordChanged,PasswordChangeDate,ChangePwdCnt from DimUserInfo
+select
+    EntityKey, UserLoginID, UserName, LoginPassword, PasswordChanged, PasswordChangeDate, ChangePwdCnt
+from DimUserInfo
 
 
 
